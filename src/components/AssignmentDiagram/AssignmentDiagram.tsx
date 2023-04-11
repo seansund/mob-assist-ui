9b0534167867db2f0d65c1ddaba0a51ac1957d99
Sean Sundberg <seansund@us.ibm.com>
2023-04-11 12:04:29 -0500

    Make assignment diagram responsive (#18)
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- src/components/AssignmentDiagram/AssignmentDiagram.tsx
+++ src/components/AssignmentDiagram/AssignmentDiagram.tsx
@@ -1,22 +1,41 @@
+
 export interface AssignmentDiagramProps {
+    parentWidth: number
+    parentHeight: number
     layers?: string[]
 }
 
 export const AssignmentDiagram = (props: AssignmentDiagramProps) => {
+
+    const width = props.parentWidth || 816;
+
+    const aspect = 816 / 440;
+
+    const height = Math.ceil(width / aspect);
+
     const layers = props.layers || []
 
     let zValue = 1;
     return (
-        <svg width="816" height="440"
+        <div style={{
+            position: 'relative',
+            overflow: 'visible',
+            height: '1px'
+        }}>
+        <svg style={{ overflow: 'visible' }}
+             preserveAspectRatio="xMinYMin slice"
+             viewBox={`0 0 ${width} ${height}`}
+            width={width} height={height}
              xmlns="http://www.w3.org/2000/svg">
             <image
                 href="/images/assignments/sanctuary-base.svg"
-                x="0" y="0" width="816" height="440" />
+                x="0" y="0" width={width} height={height} />
             {layers
                 .map(layer => "/images/assignments/sanctuary-layer.svg".replace("layer", layer))
                 .map(image =>
-                    <image href={image} x="0" y="0" z={zValue++} width="816" height="440" />
+                    <image href={image} x="0" y="0" z={zValue++} width={width} height={height} />
             )}
         </svg>
+        </div>
     )
 }
--- src/views/AssignmentDiagramView/AssignmentDiagramView.tsx
+++ src/views/AssignmentDiagramView/AssignmentDiagramView.tsx
@@ -1,13 +1,30 @@
 import {AssignmentDiagram} from "../../components/AssignmentDiagram";
 import {useParams} from "react-router-dom";
+import {useEffect, useRef, useState} from "react";
 
 export interface AssignmentDiagramViewProps {
 }
 
 export const AssignmentDiagramView = (props: AssignmentDiagramViewProps) => {
-    let { assignment } = useParams();
+    const [width, setWidth] = useState(0);
+    const [height, setHeight] = useState(0);
+
+    const { assignment } = useParams();
+    const canvasRef = useRef<any>(null)
+    useEffect(() => {
+        const resizeObserver = new ResizeObserver((event) => {
+            setWidth(event[0].contentBoxSize[0].inlineSize);
+            setHeight(event[0].contentBoxSize[0].blockSize);
+        })
+
+        resizeObserver.observe(canvasRef.current)
+    }, [canvasRef]);
 
     const layers = assignment ? assignment.toLowerCase().split(",") : []
 
-    return (<AssignmentDiagram layers={layers} />)
+    return (
+        <div style={{width: '100%'}} ref={canvasRef}>
+            <AssignmentDiagram parentWidth={width} parentHeight={height} layers={layers} />
+        </div>
+    )
 }
